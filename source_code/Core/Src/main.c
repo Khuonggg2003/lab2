9b9b33cd6e6cf6d2a3e911750902f0b12b00fa9a
Khuonggg2003 <anhkhuongg@gmail.com>
2023-11-01 01:00:35 +0700

    no message

--- source_code/Core/Src/main.c
+++ source_code/Core/Src/main.c
@@ -152,6 +152,9 @@ void display7SEG(int num){
 		break;
 	}
 }
+
+int hour = 15, minute = 8, second = 50;
+void updateClockBuffer();
 /* USER CODE END 0 */
 
 /**
@@ -192,7 +195,22 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+	  second++;
+	  if(second >= 60){
+		  second = 0;
+		  minute++;
+	  }
 
+	  if(minute >= 60){
+		  minute = 0;
+		  hour++;
+	  }
+
+	  if(hour >= 24){
+		  hour = 0;
+	  }
+	  updateClockBuffer();
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -358,21 +376,22 @@ void update7SEG(int index){
 	}
 }
 
+void updateClockBuffer(){
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
+}
+
 int dot_status = 0;
-int counter = 25;
-int counter2 = 100;
+int counter = 100;
 // index_led = enable
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 	counter--;
-	counter2--;
 	if(counter <= 0){
-		counter = 25;
+		counter = 100;
 		if(index_led >= MAX_LED) index_led = 0;
 		update7SEG(index_led++);
-	}
-
-	if(counter2 <= 0){
-		counter2 = 100;
 		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	}
 }
